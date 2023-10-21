42f7a0d419dfd1932612a6d08b7dd621d819ea17
krokowski0x <krokowski.dx@gmail.com>
2023-10-21 13:32:42 +0200

    Added Langchain SQL parser

---
+++ backend/db/sql.ts
@@ -0,0 +1,79 @@
+import { DataSource } from "typeorm";
+import { SqlDatabase } from "langchain/sql_db";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { RunnableSequence } from "langchain/schema/runnable";
+import { StringOutputParser } from "langchain/schema/output_parser";
+
+const runQuery = async (question: string) => {
+  const datasource = new DataSource({
+    type: "sqlite",
+    database: "test.db",
+  });
+
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+  });
+
+  const llm = new ChatOpenAI();
+
+  const prompt =
+    PromptTemplate.fromTemplate(`Based on the provided SQL table schema below, write a SQL query that would answer the user's question.
+  ------------
+  SCHEMA: {schema}
+  ------------
+  QUESTION: {question}
+  ------------
+  SQL QUERY:`);
+
+  const sqlQueryChain = RunnableSequence.from([
+    {
+      schema: async () => db.getTableInfo(),
+      question: (input: { question: string }) => input.question,
+    },
+    prompt,
+    llm.bind({ stop: ["\nSQLResult:"] }),
+    new StringOutputParser(),
+  ]);
+
+  const finalResponsePrompt =
+    PromptTemplate.fromTemplate(`Based on the table schema below, question, SQL query, and SQL response, write a natural language response:
+  ------------
+  SCHEMA: {schema}
+  ------------
+  QUESTION: {question}
+  ------------
+  SQL QUERY: {query}
+  ------------
+  SQL RESPONSE: {response}
+  ------------
+  NATURAL LANGUAGE RESPONSE:`);
+
+  const finalChain = RunnableSequence.from([
+    {
+      question: (input) => input.question,
+      query: sqlQueryChain,
+    },
+    {
+      schema: async () => db.getTableInfo(),
+      question: (input) => input.question,
+      query: (input) => input.query,
+      response: (input) => db.run(input.query),
+    },
+    {
+      result: finalResponsePrompt.pipe(llm).pipe(new StringOutputParser()),
+      sql: (previousStepResult) => previousStepResult.query,
+      sqlResult: (previousStepResult) => previousStepResult.response
+    },
+  ]);
+
+  const finalResponse = await finalChain.invoke({
+    question,
+  });
+
+  console.log({ finalResponse });
+
+  return finalResponse;
+};
+
+export { runQuery }
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { runQuery } from "./db/sql";
 
 dotenv.config();
 
@@ -10,68 +11,12 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
-app.post("/api/sql", (req: Request, res: Response) => {
+app.post("/api/sql", async (req: Request, res: Response) => {
   const { query } = req.body;
-  console.log(query);
 
-  const response = {
-    sqlResult: [
-      {
-        reference_number: "III RC 204/21",
-        court: "Sad Rejonowy w Gizycku",
-        judgment_date: "2021-12-30",
-        publication_date: "2022-02-08",
-        mother_income: 909,
-        father_income: 4350,
-        siblings: 2,
-        children: [
-          {
-            child_age: 4,
-            alimony_asked: 600,
-            alimony_granted: 500,
-            general_expenses_plaintiff: null,
-            general_expenses_court: 1000,
-            additional_costs: 200,
-          },
-          {
-            child_age: 2.5,
-            alimony_asked: 600,
-            alimony_granted: 400,
-            general_expenses_plaintiff: null,
-            general_expenses_court: 750,
-            additional_costs: 750,
-          },
-        ],
-      },
-      {
-        reference_number: "III RC 191/15",
-        court: "Sad Rejonowy w Zabkowicach Slaskich",
-        judgment_date: "2016-02-09",
-        publication_date: "2016-03-15",
-        mother_income: 1380,
-        father_income: 1380,
-        siblings: 2,
-        children: [
-          {
-            child_age: 4,
-            alimony_asked: 700,
-            alimony_granted: 300,
-            general_expenses_plaintiff: null,
-            general_expenses_court: 650,
-            additional_costs: null,
-          },
-          {
-            child_age: 2,
-            alimony_asked: 700,
-            alimony_granted: 300,
-            general_expenses_plaintiff: null,
-            general_expenses_court: 550,
-            additional_costs: 550,
-          },
-        ],
-      },
-    ],
-  };
+  const response = await runQuery(query)
+
+  console.log(response)
 
   res.send(response);
 });
