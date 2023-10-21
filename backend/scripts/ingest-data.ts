843a4fa5910215b57f4242bf3b083199d2af3c48
krokowski0x <krokowski.dx@gmail.com>
2023-10-21 16:48:13 +0200

    Added ingestion script

---
+++ backend/scripts/ingest-data.ts
@@ -0,0 +1,51 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+
+const filePath = 'docs';
+const NAMESPACE = "III RC 204/21"
+
+export const run = async () => {
+  try {
+    const directoryLoader = new DirectoryLoader(filePath, {
+      '.pdf': (path) => new PDFLoader(path)
+    });
+
+    const rawDocs = await directoryLoader.load();
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log('split docs', docs);
+
+    console.log('creating vector store...');
+
+    const embeddings = new OpenAIEmbeddings();
+    const pinecone = new Pinecone({
+      environment: process.env.PINECONE_ENVIRONMENT ?? '',
+      apiKey: process.env.PINECONE_API_KEY ?? '',
+    });
+    const index = pinecone.Index(process.env.PINECONE_INDEX_NAME ?? '');
+
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: NAMESPACE,
+      textKey: 'text',
+    });
+
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+};
+
+(async () => {
+  await run();
+  console.log('ingestion complete');
+})();
